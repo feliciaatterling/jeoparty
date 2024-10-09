f74d5c6df22395aa36f1a4b4c382b83d72eff01d
OlleSkold <olleskold1999@gmail.com>
2024-10-09 23:03:20 +0200

    Select team name requirement
    Fixade så att man måste välja namn på alla teams för att kunna starta ett spel

--- frontend/src/app/gamesetup/page.tsx
+++ frontend/src/app/gamesetup/page.tsx
@@ -84,11 +84,22 @@ export default function GameSetup() {
   };
 
   async function handleStartGame() {
+    // Check if all teams have names
+    const allTeamsNamed = teams
+      .slice(0, sliderValue)
+      .every((team) => team.name.trim() !== "");
+
+    if (!allTeamsNamed) {
+      alert("Please provide a name for each team before starting the game.");
+      return; // Prevent the game from starting
+    }
+
     const gameObject: gameSetup = {
       categories: categories,
       context: context,
       teams: teams.filter((team) => team.name !== ""),
     };
+
     const gameId: string = await createGame(gameObject);
     router.push(`/gamecard/${gameId}`);
   }
