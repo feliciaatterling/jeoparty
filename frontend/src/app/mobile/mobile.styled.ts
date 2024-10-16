b71c86da9176668494624f2c96413136a06e87be
Patrik Larsson <patrik@dictodesigns.com>
2024-10-16 12:02:22 +0200

    added mobile landing page and isMobile hook

---
+++ frontend/src/app/mobile/mobile.styled.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+export const ScDescriptionContainer = styled.div``;
+
+export const ScDemoContainer = styled.div`
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.5);
+`;
+
+export const ScHowToPlayContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ScFooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  text-align: center;
+`;
+
+export const ScHowToCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  background-color: #ffffff15;
+  border: 2px solid #ffffff20;
+  border-radius: 8px;
+`;
+
+export const Sc404Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+`;
--- frontend/src/app/mobile/mobile.tsx
+++ frontend/src/app/mobile/mobile.tsx
@@ -1,46 +1,33 @@
 "use client";
 import React from "react";
-import Button from "@/components/Button/Button";
+
 import Typography from "@/components/Typography/Typography";
 import {
   HomeContainer,
-  ScButtonContainer,
   ScDemoContainer,
   ScDescriptionContainer,
   ScFooterContainer,
   ScHowToPlayContainer,
-} from "./page.styled";
+} from "./mobile.styled";
 import { IoGameControllerOutline, IoHelpOutline } from "react-icons/io5";
 import { FaRegSurprise } from "react-icons/fa";
 import Logo from "@/components/Logo/Logo";
 import Spacer from "@/components/Spacer/Spacer";
 import Image from "next/image";
-import useIsMobile from "@/hooks/useIsMobile";
-import HomeMobile from "./mobile/mobile";
 import HowToCard from "@/components/HowToCard/HowToCard";
 
-export default function Home() {
-  const isMobile = useIsMobile();
-
-  const scrollToSection = () => {
-    const section = document.getElementById("how-to-play");
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth", block: "end" });
-    }
-  };
-
-  return !isMobile ? (
+export default function HomeMobile() {
+  return (
     <HomeContainer>
       <Spacer orientation="vertical" size={4} />
-      <Logo size="large" />
+      <Logo size="medium" />
       <Spacer orientation="vertical" size={4} />
 
-      {/* Game description */}
       <ScDescriptionContainer>
         <Typography
           variant="h1"
           align="center"
-          style={{ fontSize: 64, fontWeight: 600 }}
+          style={{ fontSize: 42, fontWeight: 600 }}
         >
           Who is smartest at the party?
         </Typography>
@@ -49,109 +36,77 @@ export default function Home() {
           variant="p"
           align="center"
           color="#ffffff95"
-          style={{ lineHeight: "1.15em", fontSize: 18, fontWeight: 300 }}
+          style={{ lineHeight: "1.15em", fontSize: 13, fontWeight: 300 }}
         >
           Jeoparty is an online version of the classic game Jeopardy, where all
           questions are generated with AI!
         </Typography>
       </ScDescriptionContainer>
-
       <Spacer orientation="vertical" size={3} />
 
-      <ScButtonContainer>
-        <Button
-          label="LET'S PLAY"
-          as="button"
-          onClick={() => (window.location.href = "/gamesetup")}
-        />
-        <Button
-          label="HOW TO PLAY"
-          variant="secondary"
-          as="button"
-          onClick={scrollToSection}
-        />
-      </ScButtonContainer>
-
-      <Spacer orientation="vertical" size={4} />
-      <Spacer orientation="vertical" size={3} />
-
-      <ScHowToPlayContainer id="how-to-play">
+      <ScHowToPlayContainer>
         <ScDemoContainer>
           <Image
             src="/images/mockup_jeoparty.png"
             alt="Demo video"
-            width={1100}
-            height={1100 / 2.084}
+            width={350}
+            height={350 / 2.084}
             style={{ border: "2px solid #484b72", borderRadius: 8 }}
           />
         </ScDemoContainer>
 
-        <Spacer orientation="vertical" size={4} />
+        <Spacer orientation="vertical" size={3} />
+        <Typography
+          variant="p"
+          align="center"
+          style={{ fontSize: 13, fontWeight: 300, padding: "0 12px" }}
+        >
+          We're sorry! We currently do not support mobile games, please come
+          back again on desktop or tablet!
+        </Typography>
+        <Spacer orientation="vertical" size={3} />
 
-        <Typography variant="h1" align="left" style={{ width: "100%" }}>
+        <Typography variant="h1" align="center" style={{ fontSize: 28 }}>
           How to play
         </Typography>
-
         <Spacer orientation="vertical" size={2} />
-
-        <div style={{ display: "flex", gap: 24 }}>
-          <HowToCard label="The Game" Icon={IoGameControllerOutline}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
+          <HowToCard label="The Game" Icon={IoGameControllerOutline} mobile>
             Jeoparty is a trivia game based on the classic Jeopardy format.
             There are six categories with five questions each. The more money
             the questions are worth, the more difficult they are!
           </HowToCard>
-          <HowToCard label="The Questions" Icon={IoHelpOutline}>
+          <HowToCard label="The Questions" Icon={IoHelpOutline} mobile>
             {`
       In Jeopardy, you must respond in the form of a question. If the
       question is "the first US president", your answer should be "Who
       is George Washington!"`}
           </HowToCard>
-          <HowToCard label="The Twist" Icon={FaRegSurprise}>
+          <HowToCard label="The Twist" Icon={FaRegSurprise} mobile>
             JeoParty uses AI to create unique questions and categories on the
             spot. This means every game is different, with fresh challenges
             every time you play! Try giving the AI instructions to better tailor
             your experience!
           </HowToCard>
         </div>
-        <Spacer orientation="vertical" size={4} />
 
-        <Typography
-          variant="meta"
-          align="center"
-          style={{
-            fontWeight: 300,
-            color: "#ffffff90",
-          }}
-        >
-          Ready to test your knowledge?
-        </Typography>
         <Spacer orientation="vertical" size={1} />
-
-        <div style={{ width: 275 }}>
-          <Button
-            label="LET'S PLAY"
-            as="button"
-            onClick={() => (window.location.href = "/gamesetup")}
-            style={{ padding: 16, fontSize: 18 }}
-          />
-        </div>
       </ScHowToPlayContainer>
-      <Spacer orientation="vertical" size={4} />
+      <Spacer orientation="vertical" size={3} />
+
       <ScFooterContainer>
         <Typography variant="h3">KTH Royal Institute of Technology</Typography>
         <Spacer orientation="vertical" size={0} />
 
-        <Typography variant="p" style={{ fontWeight: 300 }}>
+        <Typography variant="p" style={{ fontWeight: 300, fontSize: 14 }}>
           Project in DH2643 Advanced Interaction Programming
         </Typography>
         <Spacer orientation="vertical" size={2} />
-        <Typography variant="meta" style={{ color: "#484b72" }}>
+        <Typography variant="meta" style={{ color: "#484b72", fontSize: 12 }}>
           Developed by Felicia Atterling, Emily Nilsson, Olle Sköld, and Patrik
           Larsson 2024
         </Typography>
       </ScFooterContainer>
     </HomeContainer>
-  ) : (
-    <HomeMobile />
   );
 }
